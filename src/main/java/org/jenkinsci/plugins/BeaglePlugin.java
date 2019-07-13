5d0532b2138b3ab5513b7a7797d70714075e0304
Anees <anees.pk@appfabs.com>
2019-07-13 12:35:52 +0530

    Initial commit

---
+++ src/main/java/org/jenkinsci/plugins/BeaglePlugin.java
@@ -0,0 +1,162 @@
+package org.jenkinsci.plugins;
+
+import hudson.Launcher;
+import hudson.Extension;
+import hudson.FilePath;
+import hudson.util.FormValidation;
+import hudson.model.AbstractProject;
+import hudson.model.Run;
+import hudson.model.TaskListener;
+import hudson.tasks.Builder;
+import hudson.tasks.BuildStepDescriptor;
+import jenkins.tasks.SimpleBuildStep;
+import net.sf.json.JSONObject;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.StaplerRequest;
+import org.kohsuke.stapler.QueryParameter;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClientBuilder;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+//import org.json.simple.JSONObject;
+//import org.json.simple.parser.JSONParser;
+//import org.json.simple.parser.ParseException;
+import org.apache.http.entity.ContentType;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import org.kohsuke.stapler.export.Exported;
+
+
+public class BeaglePlugin extends Builder implements SimpleBuildStep {
+
+	String atoken;
+	String utoken;
+
+	@DataBoundConstructor
+	public BeaglePlugin(String apptoken,String usertoken) {
+		this.atoken = apptoken;
+		this.utoken = usertoken;
+	}
+
+	@Exported
+    public String getApptoken() {
+        /*StringBuffer sb = new StringBuffer(apptoken);
+        String maskedatoken = sb.replace(0,27,"XXXXXXXXXXXXXXXXXXXXXXXXXXX")
+        return maskedatoken;*/
+        return atoken;
+    }
+    @Exported
+    public String getUsertoken() {
+       /* StringBuffer sb = new StringBuffer(apptoken);
+        String maskedatoken = sb.replace(0,27,"XXXXXXXXXXXXXXXXXXXXXXXXXXX")
+        return maskedatoken;*/
+        return utoken;
+    }
+    @Exported
+    public String getGusertoken() {
+       /* StringBuffer sb = new StringBuffer(apptoken);
+        String maskedatoken = sb.replace(0,27,"XXXXXXXXXXXXXXXXXXXXXXXXXXX")
+        return maskedatoken;*/
+        return getDescriptor().getUtoken().toString();
+    }
+
+	public void perform(Run<?,?> build, FilePath workspace, Launcher launcher, TaskListener listener) {
+       	String gtoken = getDescriptor().getUtoken().toString();
+       	boolean flag = true;
+       	boolean guflag = false;
+       	if (atoken == null || atoken.isEmpty()) {
+       		listener.getLogger().println("Application Token not Provided! Refer Help File");
+       		flag = false;
+       	}
+      	if (utoken.isEmpty()) {
+      		if (gtoken.isEmpty()) {
+      			listener.getLogger().println("User Token not provided by globally or locally! Refer Help");
+       			flag = false;
+      		} else {
+      			guflag = true;
+      			utoken = gtoken;
+      		}
+       	}
+		if(flag) {
+			String body = "{\"user_token\":\""+utoken+"\",\"application_token\":\""+atoken+"\"}";
+			HttpClient c = HttpClientBuilder.create().build();   
+			HttpPost p = new HttpPost("https://beagle-tvm-api.appfabs.com/v1/test/start/");        
+			p.setEntity((HttpEntity) new StringEntity(body,ContentType.create("application/json")));
+	        HttpResponse r = null;
+			try {
+				String str = null;
+				r = c.execute(p);
+				BufferedReader rd = new BufferedReader(new InputStreamReader(r.getEntity().getContent()));
+				str = rd.readLine();
+				JsonParser parser = new JsonParser();
+				if (str != null) {
+					JsonElement jsonel = parser.parse(str);
+					JsonObject obj = jsonel.getAsJsonObject();
+					listener.getLogger().println("Status :" + obj.get("status"));
+					listener.getLogger().println("Message :" + obj.get("message"));
+					if(guflag) {
+						utoken = "";
+						guflag = false;
+					} 
+				}
+
+			} catch (IOException e) {
+			}
+		}
+		 
+    }
+	public DescriptorImpl getDescriptor() {
+        return (DescriptorImpl)super.getDescriptor();
+	}
+	@Extension
+	public static final class DescriptorImpl extends BuildStepDescriptor<Builder> {
+		public boolean isApplicable(Class<? extends AbstractProject> aClass) {
+            return true;
+		}
+		private String gutoken;
+
+		public DescriptorImpl() {
+            load();
+        }
+        @Exported
+    	public String getGusertoken() {
+       		/* StringBuffer sb = new StringBuffer(apptoken);
+        	String maskedatoken = sb.replace(0,27,"XXXXXXXXXXXXXXXXXXXXXXXXXXX")
+       		return maskedatoken;*/
+        	return gutoken;
+    	}
+		public String getDisplayName() {
+    		return "Trigger Beagle Penetration Testing";
+		}
+
+		public boolean configure(StaplerRequest req, JSONObject formData) throws FormException {
+    		gutoken = formData.get("gusertoken").toString();
+    		save();
+    		return super.configure(req,formData);
+		}
+		public String getUtoken() {
+            return gutoken;
+        }
+	}
+}
